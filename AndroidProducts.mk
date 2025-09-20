25498c7f91b5b831c339fb1577fac61cec842f4c
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2025-09-20 07:44:48 +0530

    Add Values #1
    First Edit

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_vienna.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_vienna-eng
--- device.mk
+++ device.mk
@@ -3,9 +3,25 @@ LOCAL_PATH := device/motorola/vienna
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+AB_OTA_PARTITIONS += \
+    dtbo \
+    boot \
+    vendor_dlkm \
+    init_boot \
+    system \
+    product \
+    system_ext \
+    vbmeta \
+    system_dlkm \
+    vendor \
+    vbmeta_system \
+    odm \
+    vendor_boot
+
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
@@ -14,13 +30,42 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# API levels
-PRODUCT_SHIPPING_API_LEVEL := 30
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 34
+BOARD_API_LEVEL := 34
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.enableswap:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.enableswap
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
