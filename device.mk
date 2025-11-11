aec8d4affdfad161381394022a708420c7feb0ba
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2025-11-11 23:01:16 +0530

    add decryption and reduce size

--- device.mk
+++ device.mk
@@ -7,6 +7,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 PRODUCT_TARGET_VNDK_VERSION := 34
 PRODUCT_SHIPPING_API_LEVEL := 34
+SHIPPING_API_LEVEL := 34
 PRODUCT_PLATFORM := mt6878
 PRODUCT_BOARD := mgvi_64_ww_armv82
 
@@ -19,7 +20,7 @@ AB_OTA_PARTITIONS += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 
@@ -29,10 +30,35 @@ PRODUCT_PACKAGES += \
     fastbootd
     
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-recovery \
-    android.hardware.boot@1.1-impl \
-    android.hardware.boot@1.1-service \
-    bootctrl
+    android.hardware.boot@1.2-mtkimpl-recovery \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-service \
+    bootctrl \
+    bootctrl.recovery
+    
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1 \
+    android.system.keystore2
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.crypto.dm_default_key.options_format.version=2 \
+	ro.crypto.volume.metadata.method=dm-default-key \
+	keymaster_ver=4.1
+
+PRODUCT_PACKAGES += \
+    android.hardware.security.keymint \
+    android.hardware.security.secureclock \
+    android.hardware.security.sharedsecret
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6878 \
@@ -51,3 +77,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
+    
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/vendor_ramdisk/first_stage_ramdisk/fstab.mt6878:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6878
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/vendor_ramdisk/first_stage_ramdisk/fstab.emmc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc
