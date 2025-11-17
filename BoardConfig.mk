d82fe67909240fa7e3a726546c14250516de972c
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2025-11-17 22:02:02 +0530

    h

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,7 @@
 DEVICE_PATH := device/motorola/vienna
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -13,19 +15,30 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
-TARGET_SUPPORTS_64_BIT_APPS := true
-TARGET_IS_64_BIT := true
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+TW_LOAD_VENDOR_MODULES := "goodix_gt96x_u_mmi.ko goodix_brl_u_mmi.ko touchscreen_u_mmi.ko"
+
+TARGET_OTA_ASSERT_DEVICE := vienna
 
 TARGET_BOARD_PLATFORM := mt6878
-TARGET_BOARD_PLATFORM_GPU := mali-g615mc2
+TARGET_BOOTLOADER_BOARD_NAME := mgvi_64_ww_armv82
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
 
-TW_LOAD_VENDOR_MODULES := "goodix_brl_u_mmi.ko goodix_gt96x_u_mmi.ko touchscreen_u_mmi.ko"
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_USES_MTK_HARDWARE := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 
 TARGET_NO_KERNEL := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+
 BOARD_VENDOR_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_PAGE_SIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 4
@@ -51,93 +64,149 @@ BOARD_MKBOOTIMG_ARGS += \
     --base $(BOARD_KERNEL_BASE) \
     --header_version $(BOARD_BOOT_HEADER_VERSION)
 
+TARGET_NO_RECOVERY := true
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
+
+BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_USES_VENDOR_DLKMIMAGE := true
-BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_DTB_SIZE := $(stat -L -c %s $(TARGET_PREBUILT_DTB))
+
+# Use F2FS for userdata
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Workaround for build errors with ramdisk copying
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ODM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Mount points
+TARGET_COPY_OUT_SYSTEM := system
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+TARGET_COPY_OUT_SYSTEM_DLKM := system_dlkm
+TARGET_COPY_OUT_ODM := odm
+TARGET_COPY_OUT_ODM_DLKM := odm_dlkm
+TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
-BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_RAMDISK_USE_LZ4 := true
-AB_OTA_UPDATER := true
+# ========================================
+# Metadata Partition
+# ========================================
+BOARD_USES_METADATA_PARTITION := true
 
-TARGET_NO_RECOVERY := true
-BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
-BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
+# ========================================
+# Encryption / Decryption Support (FBE)
+# ========================================
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_FSCRYPT_POLICY := 2
+TW_FORCE_KEYMASTER_VER := true
+
+# Enables proper handling of /data/media
 RECOVERY_SDCARD_ON_DATA := true
 
-TARGET_BOOTLOADER_BOARD_NAME := mgvi_64_ww_armv82
-TARGET_NO_BOOTLOADER := true
-TARGET_USES_UEFI := true
-
-BOARD_HAS_MTK_HARDWARE := true
-BOARD_USES_MTK_HARDWARE := true
-MTK_HARDWARE := true
-
-TARGET_OTA_ASSERT_DEVICE := vienna
-TW_DEFAULT_BRIGHTNESS := 1020
-TW_MAX_BRIGHTNESS := 3075
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
-
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-
-BOARD_SUPER_PARTITION_SIZE := 23085449216
-BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 23081254912
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := product vendor system system_ext
-
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_SYSTEM_EXT := system_ext
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_AVB_ENABLE := true
-
-
-#----------------------------------------------
-# Anti-Rollback Bypass (required for device)  |
-#----------------------------------------------
+# ========================================
+# Anti-Rollback Bypass
+# ========================================
 PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 PLATFORM_VERSION := 99.87.36
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
+# ========================================
+# Wipe Handling / Misc
+# ========================================
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 
-BOARD_USES_METADATA_PARTITION := true
-TW_INCLUDE_CRYPTO := false
-#TW_INCLUDE_CRYPTO_FBE := false
-#TW_INCLUDE_FBE_METADATA_DECRYPT := false
-#TW_USE_FSCRYPT_POLICY := 2
-TW_PREPARE_DATA_MEDIA_EARLY := true
+# ========================================
+# AVB (Android Verified Boot)
+# ========================================
+BOARD_AVB_ENABLE := true
 
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+# ========================================
+# FSTab
+# ========================================
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.mt6878.rc
+#TW_SKIP_ADDITIONAL_FSTAB := true
 
-BOARD_VNDK_VERSION := current
+# ========================================
+# System Properties
+# ========================================
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
+# ========================================
+# Init Support
+# ========================================
+TARGET_INIT_VENDOR_LIB := libinit_vienna
+TARGET_RECOVERY_DEVICE_MODULES := libinit_vienna
+
+# ========================================
+# Display / UI
+# ========================================
+
+# Fixes wrong theme color
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+
+# Brightness flags
+TW_MAX_BRIGHTNESS := 3075
+TW_DEFAULT_BRIGHTNESS := 1020
+TW_FRAMERATE := 60
 TW_NO_SCREEN_BLANK := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_THEME := portrait_hdpi
-TW_INCLUDE_FB2PNG := true
-TW_HAS_DOWNLOAD_MODE := false
-TW_EXCLUDE_TZDATA := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-TW_HAS_MTP := true
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-TW_EXCLUDE_TWRPAPP := true
-TW_EXCLUDE_APEX := true
 
-TW_DEFAULT_DEVICE_NAME := vienna
-TW_DEVICE_VERSION := vienna - cloud
+# CPU temp sysfs path, if it is zero all the time
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone7/temp
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TARGET_USES_MKE2FS := true
+TW_DEVICE_VERSION := v0.1.0 | cloud
+
+# ========================================
+# TWRP Tools & Features
+# ========================================
+# Core tools for filesystem and debugging
+TW_INCLUDE_FB2PNG := true  # Framebuffer screenshot support
+TW_INCLUDE_NTFS_3G := true  # NTFS read/write
+TW_INCLUDE_FUSE_EXFAT := true  # ExFAT support via fuse
+TW_INCLUDE_FUSE_NTFS := true  # NTFS via fuse
+# Resetprop (to override props)
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_LIBRESETPROP := true
+# Repack boot images
+TW_INCLUDE_REPACKTOOLS := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_LPDUMP := true
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+TW_HAS_MTP := true
+TW_USB_STORAGE := true
+
+# FastbootD
+TW_INCLUDE_FASTBOOTD := true
+
+# ========================================
+# Status Bar Customization
+# ========================================
+TW_STATUS_ICONS_ALIGN := center
+
+# ========================================
+# Debug & Logging
+# ========================================
+TWRP_INCLUDE_LOGCAT := true
+TWRP_EVENT_LOGGING := true
+TARGET_USES_LOGD := true
--- device.mk
+++ device.mk
@@ -1,15 +1,15 @@
 LOCAL_PATH := device/motorola/vienna
+DEVICE_PATH := device/motorola/vienna
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 PRODUCT_TARGET_VNDK_VERSION := 32
+BOARD_SHIPPING_API_LEVEL := 32
 PRODUCT_SHIPPING_API_LEVEL := 32
-SHIPPING_API_LEVEL := 32
 PRODUCT_PLATFORM := mt6878
 PRODUCT_BOARD := mgvi_64_ww_armv82
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
 
 AB_OTA_PARTITIONS += \
     system \
@@ -20,39 +20,44 @@ AB_OTA_PARTITIONS += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-
-
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
-    
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl-recovery \
-    android.hardware.boot@1.1-mtkimpl \
-    android.hardware.boot@1.1-service \
-    bootctrl \
-    bootctrl.recovery
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6878 \
-    bootctrl.mt6878.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl \
-    update_engine_client
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
-    update_verifier \
-    update_engine_sideload
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    keymaster_ver=4.1
+
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1 \
+    android.system.keystore2
+
+# Security
+PRODUCT_PACKAGES += \
+    android.hardware.security.keymint \
+    android.hardware.security.secureclock \
+    android.hardware.security.sharedsecret
+
+# Additional configs
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl \
+    update_engine_client
     
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
-    
-#PRODUCT_COPY_FILES += $(LOCAL_PATH)/vendor_ramdisk/first_stage_ramdisk/fstab.mt6878:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6878
-#PRODUCT_COPY_FILES += $(LOCAL_PATH)/vendor_ramdisk/first_stage_ramdisk/fstab.emmc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc
