755d8be2c2905b1c80611baa2223875cc9c1e1aa
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2025-11-20 00:04:19 +0530

    Remove gsi_keys due to no support for it in ofrp_14.1 branch

--- twrp_vienna.mk
+++ twrp_vienna.mk
@@ -5,7 +5,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
