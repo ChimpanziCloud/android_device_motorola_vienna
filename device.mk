71da0aa24b919cd1a61c1dc202f0dfaf43fc32f5
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2025-11-30 13:09:24 +0530

    remove everything related to decryption
    remove everything (will add it later)

--- device.mk
+++ device.mk
@@ -30,28 +30,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-    
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.volume.metadata.method=dm-default-key \
-    ro.crypto.dm_default_key.options_format.version=2 \
-    keymaster_ver=4.1
-
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1 \
-    android.system.keystore2
-
-# Security
-PRODUCT_PACKAGES += \
-    android.hardware.security.keymint \
-    android.hardware.security.secureclock \
-    android.hardware.security.sharedsecret
-
-# Additional configs
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl \
