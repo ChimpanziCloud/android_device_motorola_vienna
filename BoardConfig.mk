9af6e046279d2a16c69766508965a4d1604be5b4
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2025-12-22 14:12:24 +0900

    remove battery path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,7 +164,6 @@ TW_NO_SCREEN_BLANK := true
 
 # CPU temp sysfs path, if it is zero all the time
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone9/temp
-TW_CUSTOM_BATTERY_PATH := /sys/devices/platform/smart_battery/power_supply/battery/capacity
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
