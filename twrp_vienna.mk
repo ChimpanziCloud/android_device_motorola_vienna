ce87fafae10ce03862864122e09896455bdaa532
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2026-02-22 01:43:29 +0900

    twrp_vienna.mk: remove snapuserd for build error
    building snapuserd causes build error which is missing libgflags

--- twrp_vienna.mk
+++ twrp_vienna.mk
@@ -4,7 +4,6 @@ $(call inherit-product, device/motorola/vienna/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
