4f0ae1aefd7a802a238e8395c690ae7fec59201a
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2026-03-02 16:44:18 +0900

    BoardConfig.mk: Reduce Size to fix Build stopping due to large size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,7 +182,7 @@ TW_DEVICE_VERSION := v0.1.0 | cloud
 # TWRP Tools & Features
 # ========================================
 # Core tools for filesystem and debugging
-SIZE_REDUCTION_PLACEHOLDER := true
+SIZE_REDUCTION_PLACEHOLDER := false
 TW_INCLUDE_FB2PNG := $(SIZE_REDUCTION_PLACEHOLDER)  # Framebuffer screenshot support
 TW_INCLUDE_NTFS_3G := $(SIZE_REDUCTION_PLACEHOLDER)  # NTFS read/write
 TW_INCLUDE_FUSE_EXFAT := $(SIZE_REDUCTION_PLACEHOLDER)  # ExFAT support via fuse
