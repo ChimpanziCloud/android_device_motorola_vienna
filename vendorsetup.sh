cdb2c4eb035c8b6de88638e68f0fb5a8499bc959
ChimpanziCloud <ZirruLovesClouds@protonmail.com>
2026-03-11 19:59:59 +0900

    vendorsetup.sh: forgot to export variables 🥲

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,34 +17,34 @@ export OF_USE_LZMA_COMPRESSION=1
 export FOX_BASH_TO_SYSTEM_BIN=1
 
 # Build all partition tools 
-OF_ENABLE_ALL_PARTITION_TOOLS := 1
+export OF_ENABLE_ALL_PARTITION_TOOLS=1
 
 # don't spam the console with loop errors
-OF_LOOP_DEVICE_ERRORS_TO_LOG := 1
+export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
 
 # Called just before formatting /data; only useful for devices/ROMs that have dynamic partitions
-OF_USE_DMCTL := 1
+export OF_USE_DMCTL=1
 
 # Set this to 1 to disable automatic rebooting after openrecoveryscript finishes
-OF_DISABLE_ORS_AUTO_REBOOT := 1
+export OF_DISABLE_ORS_AUTO_REBOOT=1
 
 # ensure that /sdcard is bind-unmounted before f2fs data repair or format
-OF_UNBIND_SDCARD_F2FS := 1
+export OF_UNBIND_SDCARD_F2FS=1
 
 # avoid MTP issues after data format
-OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
+export OF_BIND_MOUNT_SDCARD_ON_FORMAT=1
 
 # Set this to 1 to force the selection of f2fs when formatting data
-OF_FORCE_DATA_FORMAT_F2FS := 1
+export OF_FORCE_DATA_FORMAT_F2FS=1
 
 # Set this to 1 to include an addon for removing factory reset protection (FRP)
-OF_ENABLE_FRP_ADDON := 1
+export OF_ENABLE_FRP_ADDON=1
 
 # Set to the maintainer's name
-OF_MAINTAINER := cloud
+export OF_MAINTAINER=cloud
 
 # Use this to change the default time zone
-OF_DEFAULT_TIMEZONE := IST-5:30
+export OF_DEFAULT_TIMEZONE=IST-5:30
 
 # Device specific
 export OF_SCREEN_H=2400
